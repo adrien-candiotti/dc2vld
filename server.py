63e9ef8990b6f2d24880cff1fb7c943fc2ebf39a
Adrien Candiotti <adrien.candiotti@tactill.com>
2016-04-04 11:36:34 +0200

    mod - now removing frontend along with backends etcd

--- server.py
+++ server.py
@@ -28,10 +28,6 @@ etcd_client = etcd.Client(host=etcd_hostname) # FIXME : protocol https?
 
 event_manager = Events()
 
-events = []
-
-containers = {}
-
 def on_open():
     logging.warning('Connection inited with docker cloud api')
 
@@ -55,52 +51,54 @@ def get_container_hostname(container):
        hostname = '%s.%s' % (hostname, stack)
     return hostname
 
-def create_backend(backend_name):
-    key = '/vulcand/backends/%s/backend' % backend_name
+def upsert(key, value, message):
     try:
         etcd_client.read(key)
         return True
     except etcd.EtcdKeyNotFound:
-        value = '{"Type": "http"}' # FIXME : https
         etcd_client.write(key, value)
-        logging.warning('Created backend : %s' % key)
+        logging.warning(message)
         return False
+
+def remove(key, message):
+    try:
+      etcd_client.delete(key)
+      logging.warning(message)
+      return True
+    except etcd.EtcdKeyNotFound:
+      logging.warning('Could not remove unexisting key: %s' % key)
+      return False
+
+def create_backend(backend_name):
+    key = '/vulcand/backends/%s/backend' % backend_name
+    value = '{"Type": "http"}' # FIXME : https
+
+    return upsert(key, value, 'Created backend : %s' % key)
 
 def add_https_redirect(backend_name):
     key = '/vulcand/frontends/%s/middlewares/http2https' % backend_name
-    try:
-        etcd_client.read(key)
-        return True
-    except etcd.EtcdKeyNotFound:
-        value = '{"Type": "rewrite", "Middleware":{"Regexp": "^http://(.*)$", "Replacement": "https://$1", "Redirect": true}}'
-        etcd_client.write(key, value)
-        logging.warning('Added https redirect middleware : %s' % key)
-        return False
+    value = '{"Type": "rewrite", "Middleware":{"Regexp": "^http://(.*)$", "Replacement": "https://$1", "Redirect": true}}'
+
+    return upsert(key, value, 'Added https redirect middleware : %s' % key)
 
 def add_rate_limiting(backend_name):
     key = '/vulcand/frontends/%s/middlewares/rate' % backend_name
-    try:
-        etcd_client.read(key)
-        return True
-    except etcd.EtcdKeyNotFound:
-        value = '{"Type": "ratelimit", "Middleware":{"Requests": 100, "PeriodSeconds": 1, "Burst": 3, "Variable": "client.ip"}}'
-        etcd_client.write(key, value)
-        logging.warning('Added rate limiting middleware : %s' % key)
-        return False
+    value = '{"Type": "ratelimit", "Middleware":{"Requests": 100, "PeriodSeconds": 1, "Burst": 3, "Variable": "client.ip"}}'
+
+    return upsert(key, value, 'Added rate limiting middleware : %s' % key)
 
 def create_frontend(backend_name, ROUTE):
     key = '/vulcand/frontends/%s/frontend' % backend_name
-    try:
-        etcd_client.read(key)
-        return True
-    except etcd.EtcdKeyNotFound:
-        # NOTE : Route could be passed as a raw string.
-        #        More flexible but not needed
-        value = '{"Type": "http", "BackendId": "%s", "Route": "PathRegexp(`%s.*`)"}'\
-                % (backend_name, ROUTE) # FIXME : https
-        etcd_client.write(key, value)
-        logging.warning('Created frontend : %s' % key)
-        return False
+    # NOTE : Route could be passed as a raw string.
+    #        More flexible but not needed
+    value = '{"Type": "http", "BackendId": "%s", "Route": "PathRegexp(`%s.*`)"}'\
+            % (backend_name, ROUTE) # FIXME : https
+ 
+    return upsert(key, value, 'Created frontend : %s' % key)
+
+def remove_frontend(backend_name)
+    key = '/vulcand/frontends/%s/frontend' % backend_name
+    remove(key, 'remove frontend : %s' % backend_name)
 
 def add_container(container):
     server_name = container.name
@@ -119,12 +117,18 @@ def add_container(container):
     ROUTE = get_envvar(container, 'ROUTE')
 
     if PORT:
+        if not ROUTE:
+          logger.warning('No route could be found for this container')
+          return
+
         key = '/vulcand/backends/%s/servers/%s' % (backend_name, server_name)
         value = '{"URL": "http://%s:%s"}' % (HOSTNAME, PORT)
 
         etcd_client.write(key, value)
         logging.warning('Added server: %s = %s on route %s' % (key, value, ROUTE))
+
         create_frontend(backend_name, ROUTE)
+        add_rate_limiting(backend_name)
         add_https_redirect(backend_name)
     else:
         logging.warning('No port could be found for this container' + container_name)
@@ -137,6 +141,8 @@ def remove_container(container):
     try:
         etcd_client.delete(key)
         logging.warning('Removed server: %s' % key)
+
+        remove_frontend(backend_name)
     except etcd.EtcdKeyNotFound as e:
         logging.error(e)
 
